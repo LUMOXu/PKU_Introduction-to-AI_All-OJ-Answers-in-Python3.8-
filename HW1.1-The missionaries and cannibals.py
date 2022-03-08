b1deebe943d2777af82a15466f0efc3857365097
LUMO_Xu <100847686+LUMOXu@users.noreply.github.com>
2022-03-09 03:41:47 +0800

    Add files via upload

---
+++ HW1.1-The missionaries and cannibals.py
@@ -0,0 +1,101 @@
+"""
+PKU, INTRODUCTION TO AI
+LECTURE 5 - GLOBAL SEARCH - HW 01 - The Missionaries and Cannibals
+
+DESCRIPTION
+The missionaries and cannibals problem is usually stated as follows: Three missionaries and three cannibals are on one
+side of a river, along with a boat that can hold one or two people. Find a way to get everyone to the other side,
+without ever leaving a group of missionaries in one place (including the boat) outnumbered by the cannibals
+in that place. Now the problem has been extended to be more complicated. There are m missionaries and m cannibals
+who want to cross the river. And the boat is also enlarged to be capable of supporting n people. In order to make
+all of them cross the river safely what is the least number of steps? Notice that when the boat goes across the
+river there must be at least one missionary or cannibal on the boat.
+
+INPUT
+The input consists of only one line, and is made of two positive number m and n, where m ≤ 1000 and n ≤ 100.
+
+OUTPUT
+Output the least number of steps needed. If the problem can't be solved, print -1 as the result.
+
+SAMPLE INPUT
+3 2
+
+SAMPLE OUTPUT
+11
+"""
+import queue
+
+
+def check(state, d_mis, d_can):
+    mis = state[1][0]
+    can = state[1][1]
+    boat = state[1][2]  # 0rtl 1ltr
+    if mis and mis < can:
+        return False
+    if boat:  # start to goal
+        if m - mis < d_mis or m - can < d_can:
+            return False
+        if m - mis > d_mis and m - mis - d_mis < m - can - d_can:
+            return False
+        if mis + d_mis > 0 and mis + d_mis < can + d_can:
+            return False
+        if tuple((state[1][0] + d_mis, state[1][1] + d_can, 0)) in close_nodes:
+            return False
+        return True
+    else:  # goal to start
+        if mis < d_mis or can < d_can:
+            return False
+        if mis > d_mis and mis - d_mis < can - d_can:
+            return False
+        if m - mis + d_mis > 0 and m - mis + d_mis < m - can + d_can:
+            return False
+        if tuple((state[1][0] - d_mis, state[1][1] - d_can, 1)) in close_nodes:
+            return False
+        return True
+
+
+def update(state, d_mis, d_can):
+    # f = 1  # you can try this f instead to see the difference
+    if state[1][2]:  # start to goal
+        f = 2 * (m - state[1][0] - d_mis) + 1 * (m - state[1][1] - d_can) - 2 * 0
+        return tuple((f, (state[1][0] + d_mis, state[1][1] + d_can, 0, state[1][3] + 1)))
+    else:  # goal to start
+        f = 2 * (m - state[1][0] + d_mis) + 1 * (m - state[1][1] + d_can) - 2 * 1
+        return tuple((f, (state[1][0] - d_mis, state[1][1] - d_can, 1, state[1][3] + 1)))
+
+
+def bfs(m, n):
+    # nodes = 0
+    while not open_nodes.empty():
+        current_state = open_nodes.get()
+        close_nodes.add(current_state[1][:3])
+        # nodes += 1
+        if current_state[1][:2] == (m, m):  # FINISH! =)
+            # print(nodes)  # debug, output the number of closed nodes
+            return current_state[1][3]  # which is the step_count
+        for i in range(1, n + 1):  # the total count of human on boat
+            for j in range(0, i + 1):  # cannibals
+                if check(current_state, i - j, j):  # do the state satisfy the rules after update?
+                    open_nodes.put(update(current_state, i - j, j))  # If so, update the state.
+
+    return -1  # can't find an answer =(
+
+
+m, n = map(int, input().split())
+open_nodes = queue.PriorityQueue()
+open_nodes.put((3 * m - 2, (0, 0, 1, 0)))
+# a status <- (f value, (missionaries at goal, cannibals at goal, boat state(0 means right to left, vice versa), steps))
+close_nodes = set()
+close_nodes.add((0, 0, 1))
+# (missionaries at goal, cannibals at goal, boat status)
+print(bfs(m, n))
+
+"""
+note:
+Due to strange reasons, the priority queue didn't sort all nodes about their f-s well. However, the program still
+ran correctly and satisfied the time and space limits.
+The most important part is the function f. The order of scanning i-s and j-s are not that important.
+Given the correct f, The length of the close_nodes_set is usually the same(sometimes outnumbers a little bit) as the 
+number of the steps, which means the program can act almost perfectly at finding the shortest route 
+instead of doing simple dfs searching. (Doing simple dfs in python always results in TLE)
+"""
---
+++ HW1.2-Sudoku.py
@@ -0,0 +1,114 @@
+"""
+PKU, INTRODUCTION TO AI
+LECTURE 5 - GLOBAL SEARCH - HW 02 - Sudoku
+
+DESCRIPTION
+Sudoku is a very simple task. A square table with 9 rows and 9 columns is divided to 9 smaller squares 3x3 as shown
+on the Figure. In some of the cells are written decimal digits from 1 to 9. The other cells are empty. The goal is to
+fill the empty cells with decimal digits from 1 to 9, one digit per cell, in such way that in each row, in each column
+and in each marked 3x3 subsquare, all the digits from 1 to 9 to appear. Write a program to solve a given Sudoku-task.
+
+INPUT
+The input data will start with the number of the test cases. For each test case, 9 lines follow, corresponding to
+the rows of the table. On each line a string of exactly 9 decimal digits is given, corresponding to the cells
+in this line. If a cell is empty it is represented by 0.
+
+OUTPUT
+For each test case your program should print the solution in the same format as the input data. The empty cells
+have to be filled according to the rules. If solutions is not unique, then the program may print any one of them.
+
+SAMPLE INPUT
+1
+103000509
+002109400
+000704000
+300502006
+060000050
+700803004
+000401000
+009205800
+804000107
+
+SAMPLE OUTPUT
+143628579
+572139468
+986754231
+391542786
+468917352
+725863914
+237481695
+619275843
+854396127
+"""
+
+
+def find_number(num):
+    global a, x, end_flag
+    if num == 81:  # fill ended, get back
+        end_flag = True
+        return 'いいよ！こいよ！'
+    if a[num] != 0:  # this blank[num] is already filled in the question, so go to blank[num+1]
+        find_number(num + 1)
+        return 'いいよ！こいよ！'  # if blank[num+1] can't be filled and blank[num] is already filled
+        # in this question, return here.
+    else:
+        for k in range(9):  # put in k+1, not k!
+            a[num] = k+1  # set blank[num] to k+1
+            if row[num // 9][k]:  # can we fill this number(k+1) in this blank? If not,
+                # clear the number and get back to blank[num-1]
+                a[num] = 0
+                continue
+            if col[num % 9][k]:
+                a[num] = 0
+                continue
+            if gong[num // 9 // 3 * 3 + num % 9 // 3][k]:
+                a[num] = 0
+                continue
+            row[num // 9][k] = True  # if can, all occupy modes of blank[num] set to True
+            col[num % 9][k] = True
+            gong[num // 9 // 3 * 3 + num % 9 // 3][k] = True
+
+            find_number(num + 1)  # fill in the next blank
+
+            if not end_flag:  # if the the blank[num+1] can't be filled, return here in blank[num]
+                a[num] = 0  # reset blank[num] to 0
+                row[num // 9][k] = False  # clear the occupy modes of blank[num]
+                col[num % 9][k] = False
+                gong[num // 9 // 3 * 3 + num % 9 // 3][k] = False
+            else:  # fill ended
+                return 'いいよ！こいよ！'  # back to previous num, eventually ends the function
+
+
+def solve():
+    find_number(0)  # start filling from the first blank(blank[0] or a[0])
+    return 'いいよ！こいよ！'
+
+
+def print_sudoku(sudoku):  # convert the sudoku list to answer form
+    for i in range(1, 10):
+        print(''.join(map(str, sudoku[9 * (i - 1): 9 * i])))
+    return 'いいよ！こいよ！'
+
+
+n = int(input())
+for i in range(n):
+    end_flag = False
+    x = []
+    row = [[False for s in range(9)] for j in range(9)]  # row[i][j] means whether there's j+1 in the i+1-th row
+    col = [[False for s in range(9)] for j in range(9)]  # the same for col and gong
+    gong = [[False for s in range(9)] for j in range(9)]
+    for j in range(9):
+        x.extend(list(map(int, list(input()))))  # convert the sudoku to a list(81 long)
+    for j in range(81):  # scan the list, if the a blank has been filled (x[j]!=0), set its all occupy modes to True.
+        if x[j]:
+            row[j // 9][x[j] - 1] = True
+            col[j % 9][x[j] - 1] = True
+            gong[j // 9 // 3 * 3 + j % 9 // 3][x[j] - 1] = True
+    a = x.copy()
+    solve()
+    print_sudoku(a)
+
+
+"""
+note: Simple dfs algorithm. An ordinary Sudoku takes 0.9-100 ms.
+"""
